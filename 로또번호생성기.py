54eef739f9a4c4f4bf086e8bec51e88e98dd1c4e
nowaveosu <82007474+nowaveosu@users.noreply.github.com>
2022-03-03 10:15:31 +0900

    Add files via upload

--- 로또번호생성기.py
+++ 로또번호생성기.py
@@ -4,10 +4,12 @@ import random
 numbers = []
 for i in range(1,46):
     numbers.append(i)
-
+result = []
 print("밤무리의 로또추천번호는~~")
 for i in range(6):
-    print(random.choice(numbers), end = ' ' )
-    
-    if i == 5:
-        print("입니다!")
+    result.append(random.choice(numbers))
+#중복제거
+result = list(set(result))
+if len(result) < 6:
+    result.append(random.choice(numbers))
+print(result, "입니다!")
